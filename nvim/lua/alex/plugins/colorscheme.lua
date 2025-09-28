d407ac9ed0c7bc1b440bac8b54c7e7bc409a1d81
alex <a.leminiscata@gmail.com>
2025-09-28 15:04:16 -0700

    Adding extra themes

--- nvim/lua/alex/plugins/colorscheme.lua
+++ nvim/lua/alex/plugins/colorscheme.lua
@@ -1,7 +1,14 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme onedark]])
-  end
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme onedark]])
+    end
+  },
+  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'sainnhe/gruvbox-material',
+  'EdenEast/nightfox.nvim',
+  'sainnhe/everforest'
 }
